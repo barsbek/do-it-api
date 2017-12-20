a00287ebc3dec86cadb9e96a85bd4b2f4213d55f
barsbek <mak.bekmamat@gmail.com>
2017-12-20 07:44:10 +0500

    user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:email, :name, :password_digest)
+      params.require(:user).permit(:email, :name, :password, :password_confirmation)
     end
 end
