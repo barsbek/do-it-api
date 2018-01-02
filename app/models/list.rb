839792a42f14ab4e7c70dd8e63221e3346dd9263
Unknown <mak.bekmamat@gmail.com>
2018-01-02 15:28:55 +0500

    increase pagination and destroy dependent

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
   default_scope { order(created_at: :desc) }
   belongs_to :collection
-  has_many :tasks
-  paginates_per 10
+  has_many :tasks, dependent: :destroy
+  paginates_per 50
 end
