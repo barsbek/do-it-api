77b2bf15215e11f7710b8f483c2b39269e64edf4
barsbek <mak.bekmamat@gmail.com>
2018-01-11 02:06:22 +0500

    validate email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,15 @@ class User < ApplicationRecord
   has_many :lists, dependent: :destroy
   has_many :tasks
 
+
   has_attached_file :avatar, styles: { thumb: "60x60#" }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
-  validates_presence_of :email
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, length: {maximum: 250},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: true,
+    presence: true
 
   def avatar_thumb
     avatar.url(:thumb)
